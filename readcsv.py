7529075c97d991a41f2f6b4a8b4332a3ba21f266
jyyang42 <yjykitty.yang@gmail.com>
2018-12-01 18:26:03 -0500

    updated csvreader.

--- readcsv.py
+++ readcsv.py
@@ -1,13 +1,15 @@
 import csv
 
 def readcsv(name):
-    dic = {}
+    id_l_b=[[],[],[]]
     with open(name) as csvfile:
         reader = csv.DictReader(csvfile)
         
         for row in reader:
-            dic[row['source_id']] = (row['l'],row['b'])
-    return dic
+            id_l_b[0].append(row['source_id'])
+            id_l_b[1].append(row['l'])
+            id_l_b[2].append(row['b'])
+    return id_l_b
 
 if __name__ == "__main__":
     print(readcsv('GaiaSource-1000172165251650944-1000424567594791808.csv'))
