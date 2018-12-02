a2ac2351b0a410925b748c853d2dd727d4dc93f4
Matthew Groll <43278304+matthewgroll@users.noreply.github.com>
2018-12-01 22:45:26 -0500

    Update grapher.py

--- grapher.py
+++ grapher.py
@@ -30,3 +30,27 @@ def readcsv(name):
 # b is galactic latitude
 
 star_dicts = readcsv('GaiaSource-1000172165251650944-1000424567594791808.csv')
+
+# plt.plot([star_dicts[1]], [star_dicts[2]], 'ro', markersize=1)
+# plt.axis([158, 160, 22, 24])
+# plt.show()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+
+xs, ys, zs = [star_dicts[1][i] for i in range(lim)], [star_dicts[2][j] for j in range(lim)], \
+             [star_dicts[3][k] for k in range(lim)]
+
+colors = cm.rainbow(np.linspace(1, 0, lim))
+for y, c in zip(star_dicts[4], colors):
+    ax.scatter(xs, ys, zs, c=colors, marker='o', s=1)
+ax.set_zlim3d(-30, 30)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
+
+
